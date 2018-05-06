f713c20c096d0439fc785a449fa1f473c2688445
Jingwei Zhang <zhang@informatik.uni-freiburg.de>
2018-05-06 18:57:47 +0200

    * set default w/ amsgrad=True in sharedAdam

--- optims/sharedAdam.py
+++ optims/sharedAdam.py
@@ -10,8 +10,8 @@ class SharedAdam(optim.Adam):
     """
 
     def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
-                 weight_decay=0):
-        super(SharedAdam, self).__init__(params, lr, betas, eps, weight_decay)
+                 weight_decay=0, amsgrad=True):
+        super(SharedAdam, self).__init__(params, lr, betas, eps, weight_decay, amsgrad)
 
         # State initialisation (must be done before step, else will not be shared between threads)
         for group in self.param_groups:
